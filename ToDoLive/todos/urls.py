26a90434149ea62f8502090494aeab2c5a900eee
ashwinPy <ashwinam31@gmail.com>
2023-08-01 21:03:41 +0530

    ToDoLive project is finished

--- ToDoLive/todos/urls.py
+++ ToDoLive/todos/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.ToDoView.as_view()),
     path('', views.ToDOClassView.as_view()),
     path('<int:pk>/', views.ToDoDetailView.as_view()),
-
+    path('todos/', views.ToDoView.as_view()),
+    path('todos/<int:pk>/', views.ToDoDetail.as_view()),
 
 ]
-
-
--- ToDoLive/todos/views.py
+++ ToDoLive/todos/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
@@ -5,22 +6,45 @@ from rest_framework import status
 from .serializers import ToDoSeriliazer
 from .models import TodoModel
 
+
 class ToDoView(APIView):
 
     def get(self, request):
         queryset = TodoModel.objects.all()
         serializer = ToDoSeriliazer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
     def post(self, request):
         serializer = ToDoSeriliazer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+
+
+class ToDoDetail(APIView):
+
+    def get(self, request, pk):
+        instance = get_object_or_404(TodoModel, pk=pk)
+        serializer = ToDoSeriliazer(instance=instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        instance = get_object_or_404(TodoModel, pk=pk)
+        serializer = ToDoSeriliazer(instance=instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        instance = get_object_or_404(TodoModel, pk=pk)
+        instance.delete()
+        return Response('{message: Object deleted successfully.}', status=status.HTTP_204_NO_CONTENT)
+
+
 class ToDOClassView(ListCreateAPIView):
     queryset = TodoModel.objects.all()
     serializer_class = ToDoSeriliazer
+
 
 class ToDoDetailView(RetrieveUpdateDestroyAPIView):
     queryset = TodoModel.objects.all()
