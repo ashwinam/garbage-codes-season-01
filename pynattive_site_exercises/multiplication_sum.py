802c29f882318302f57fd3611573969c07959942
ashwinPy <ashwinam31@gmail.com>
2023-08-01 20:35:11 +0530

    Comment changed

--- pynattive_site_exercises/multiplication_sum.py
+++ pynattive_site_exercises/multiplication_sum.py
@@ -1,7 +1,7 @@
 # Given two integer numbers return their product only if the product is equal to or lower than 1000, else return their sum.
 
 # calculate the product
-# check the product >= 1000, then return product else sum of two numbers
+# check the product <= 1000, then return product else sum of two numbers
 
 number1 = 40
 number2 = 30
