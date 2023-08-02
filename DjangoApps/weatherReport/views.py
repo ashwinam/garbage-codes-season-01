26f72de46aabd6e6f082d501bdf105613a489924
ashwinPy <ashwinam31@gmail.com>
2023-08-02 22:55:44 +0530

    settting up a weather app

--- DjangoApps/weatherReport/views.py
+++ DjangoApps/weatherReport/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+
+def Index(request):
+    return HttpResponse('Welcome to the weather report.')
+
+# Tomorrow register a weatherapi.com get a key and read about  django.core.wsgi requests module
+
+# create a url str:city_name, or find how to pass the data from frontend side
