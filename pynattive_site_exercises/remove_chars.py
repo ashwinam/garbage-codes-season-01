ac1279843ad8dec88cd4f8c7f1521101f48883eb
ashwinPy <ashwinam31@gmail.com>
2023-08-03 08:48:07 +0530

    Execise 4 done

---
+++ pynattive_site_exercises/remove_chars.py
@@ -0,0 +1,25 @@
+# Exercise 4: Remove first n characters from a string
+
+# Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+'''
+Analysis:
+'Pynative' 4 
+o/p = tive
+
+if index >= 4:
+add character to the output container
+'''
+
+
+def remove_char(string, no):
+    output = ''
+    for ind in range(len(string)):
+        if ind >= no:
+            output += string[ind]
+
+    return output
+
+
+print(remove_char('pynative', 4))
+print(remove_char('pynative', 2))
