401777f5dd14a07b8f818de4a3b2d594d92c6488
ashwinPy <ashwinam31@gmail.com>
2023-08-03 23:11:11 +0530

    Weather report city wise

--- DjangoApps/weatherReport/urls.py
+++ DjangoApps/weatherReport/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index)
+    path('<str:city_name>/', views.city_wise_weather)
 ]
--- DjangoApps/weatherReport/views.py
+++ DjangoApps/weatherReport/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import requests
 
 
-def Index(request):
-    return HttpResponse('Welcome to the weather report.')
+@api_view()
+def city_wise_weather(request, city_name):
+    base_url = 'http://api.weatherapi.com/v1/current.json'
 
-# Tomorrow register a weatherapi.com get a key and read about  django.core.wsgi requests module
+    params = {
+        "key": '5cc53c5ddc5d4dc7860160219230308',
+        "q": city_name
+    }
+    response = requests.get(base_url, params=params)
 
-# create a url str:city_name, or find how to pass the data from frontend side
+    if response.status_code == 200:
+
+        return Response(response.json())
+
+    return Response({'message': 'Welcome to the weather report.'})
