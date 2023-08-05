fa1e4823e289ca2895f74a4501ea51606b637422
ashwinPy <ashwinam31@gmail.com>
2023-08-05 15:19:39 +0530

    jokes model

--- DjangoApps/jokesApi/models.py
+++ DjangoApps/jokesApi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class JokesModel(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    categories = models.CharField(max_length=100)
