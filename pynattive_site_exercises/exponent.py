26eb531a107c61223b40998cdc2c9204121f500f
ashwinPy <ashwinam31@gmail.com>
2023-08-10 08:57:44 +0530

    Exercise 15

---
+++ pynattive_site_exercises/exponent.py
@@ -0,0 +1,24 @@
+# Exercise 15: Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+
+# Note here exp is a non-negative integer, and the base is an integer.
+
+'''
+5 ^ 3 = 5 * 5 * 5 = 125
+
+'''
+
+
+def exponent(base, exp):
+    output = 0
+    for i in range(exp):
+        if i == 0:
+            output = base
+        else:
+            output = output * base
+    return output
+
+
+base = 4
+exp = 3
+
+print(f'{base} rasises to the power of {exp}: {exponent(base, exp)} ')
