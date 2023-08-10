83370dd852ccfc0bc7f28cd6b0267e8d264e2442
ashwinPy <ashwinam31@gmail.com>
2023-08-10 09:00:47 +0530

    Exercise 15 better solution

--- pynattive_site_exercises/exponent.py
+++ pynattive_site_exercises/exponent.py
@@ -22,3 +22,19 @@ base = 4
 exp = 3
 
 print(f'{base} rasises to the power of {exp}: {exponent(base, exp)} ')
+
+
+# other solution
+
+def exponent1(bse, ex):
+    num = exp
+    output = 1
+
+    while num > 0:
+        output *= bse
+        num -= 1
+
+    return output
+
+
+print(exponent1(5, 3))
