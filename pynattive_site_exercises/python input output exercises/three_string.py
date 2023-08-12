332d1b63d3e8f4d433ca818010f26ad1d22399ca
ashwinPy <ashwinam31@gmail.com>
2023-08-12 08:37:29 +0530

    i/o exercise 2

---
+++ pynattive_site_exercises/python input output exercises/three_string.py
@@ -0,0 +1,11 @@
+# Exercise 2: Display three string “Name”, “Is”, “James” as “Name**Is**James”
+
+# Use the print() function to format the given words in the mentioned format. Display the ** separator between each string.
+
+'''
+Expected Output:
+
+For example: print('Name', 'Is', 'James') will display Name**Is**James
+'''
+
+print('Name', 'Is', 'James', sep='**')
