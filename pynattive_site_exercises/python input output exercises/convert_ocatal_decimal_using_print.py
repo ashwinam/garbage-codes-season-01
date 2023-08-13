8316dc7d9a5237d0d4aa2b20082780cf41f90ca7
ashwinPy <ashwinam31@gmail.com>
2023-08-13 11:02:50 +0530

    i/o exercise 3

---
+++ pynattive_site_exercises/python input output exercises/convert_ocatal_decimal_using_print.py
@@ -0,0 +1,10 @@
+# Exercise 3: Convert Decimal number to octal using print() output formatting
+
+
+'''Expected Output:
+
+The octal number of decimal number 8 is 10
+
+'''
+
+print('The octal of 15 is %o' % 15)
