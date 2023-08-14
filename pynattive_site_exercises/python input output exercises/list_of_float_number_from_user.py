02dedf5bb3ca57bc7e6d33c45edd417b286dcf1f
ashwinPy <ashwinam31@gmail.com>
2023-08-14 10:25:48 +0530

    Exercise 5 different solution

--- pynattive_site_exercises/python input output exercises/list_of_float_number_from_user.py
+++ pynattive_site_exercises/python input output exercises/list_of_float_number_from_user.py
@@ -8,3 +8,16 @@ take the input from user using spaces
 user_list = input('Provide a multiple input using spaces: ')
 
 print([float('%.2f' % float(num)) for num in user_list.split(' ')])
+
+# if user want to put single numbers at a time
+
+numbers = []
+
+for num in range(5):
+
+    print('Enter the numbers at this location ', num, ':')
+    number = float(input())
+
+    numbers.append(number)
+
+print(numbers)
