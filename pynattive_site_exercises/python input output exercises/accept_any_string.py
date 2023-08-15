6a1f27d15ab5e38b5bac831fd3b68200f920071a
ashwinPy <ashwinam31@gmail.com>
2023-08-15 19:21:37 +0530

    i/o exercise 7 second solution

--- pynattive_site_exercises/python input output exercises/accept_any_string.py
+++ pynattive_site_exercises/python input output exercises/accept_any_string.py
@@ -12,3 +12,10 @@ user_input = input('Enter three string: ')
 
 for name in user_input.split(' '):
     print('Name: ', name)
+
+
+# using unpacking values in multiple variables
+str1, str2, str3 = input("Enter three string").split()
+print('Name1:', str1)
+print('Name2:', str2)
+print('Name3:', str3)
