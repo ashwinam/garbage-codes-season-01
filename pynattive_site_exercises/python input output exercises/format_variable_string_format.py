29c0e9e671c27d716bd646d9e5a3095058b14af8
ashwinPy <ashwinam31@gmail.com>
2023-08-16 08:06:58 +0530

    i/o exercise 7

---
+++ pynattive_site_exercises/python input output exercises/format_variable_string_format.py
@@ -0,0 +1,8 @@
+# Exercise 8: Format variables using a string.format() method.
+
+totalMoney = 1000
+quantity = 3
+price = 500
+
+print('I have {} dollars so I can buy {} football for {}. dollars'. format(
+    totalMoney, quantity, price))
