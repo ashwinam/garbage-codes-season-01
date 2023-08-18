c364078b65279fdb1f34418fddf81b07ec74dc16
ashwinPy <ashwinam31@gmail.com>
2023-08-18 08:28:11 +0530

    loop exercise 2 other solution

--- pynattive_site_exercises/python loop exercises/print_pattern01.py
+++ pynattive_site_exercises/python loop exercises/print_pattern01.py
@@ -20,3 +20,20 @@ for row in range(5):
     for col in range(row + 1):
         print(col + 1, end=' ')
     print()
+
+# solution from pynative
+
+print("Number Pattern ")
+
+# Decide the row count. (above pattern contains 5 rows)
+row = 5
+# start: 1
+# stop: row+1 (range never include stop number in result)
+# step: 1
+# run loop 5 times
+for i in range(1, row + 1, 1):
+    # Run inner loop i+1 times
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    # empty line after each row
+    print("")
