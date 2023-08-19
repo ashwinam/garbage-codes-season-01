e58acbf38fa245c41fde13a9787c37e33bc15726
ashwinPy <ashwinam31@gmail.com>
2023-08-19 09:57:01 +0530

    loop exercise 5 other solution

--- pynattive_site_exercises/python loop exercises/display_numbers_from_list.py
+++ pynattive_site_exercises/python loop exercises/display_numbers_from_list.py
@@ -17,3 +17,16 @@ for num in lst:
         if num > 150:
             continue
         print(num)
+
+# Other solution
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
+# iterate each item of a list
+for item in numbers:
+    if item > 500:
+        break
+    elif item > 150:
+        continue
+    # check if number is divisible by 5
+    elif item % 5 == 0:
+        print(item)
