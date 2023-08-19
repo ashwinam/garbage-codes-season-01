96327ff2f4a80d4ebe6d7930d577d6d97f13e580
ashwinPy <ashwinam31@gmail.com>
2023-08-19 13:19:57 +0530

    file structure creating

---
+++ js-learning/js-info/js_fundamentals/task1.js
@@ -0,0 +1,17 @@
+// Task 
+    // 1. working with variables
+    /* 
+    1. Declare two variables: admin and name.
+    2. Assign the value "John" to name.
+    3. Copy the value from name to admin.
+    4. Show the value of admin using alert (must output “John”).
+
+    */
+
+    let admin;
+    let name;
+
+    name = 'john';
+
+    admin = name;
+    alert(admin);
