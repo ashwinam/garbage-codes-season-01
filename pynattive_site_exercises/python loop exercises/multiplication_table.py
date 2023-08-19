80464a3d3959110c6fde0f6e2d5039f6869f842c
ashwinPy <ashwinam31@gmail.com>
2023-08-19 09:38:53 +0530

    loop exercise 4

---
+++ pynattive_site_exercises/python loop exercises/multiplication_table.py
@@ -0,0 +1,21 @@
+# Exercise 4: Write a program to print multiplication table of a given number
+
+'''
+For example, num = 2 so the output should be
+
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+'''
+
+user_inp = int(input('Enter a number: '))
+
+for num in range(1, 11):
+    print(user_inp * num)
