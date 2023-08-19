25a04a456121afd34bbd659e0976c75684ae7a02
ashwinPy <ashwinam31@gmail.com>
2023-08-19 20:47:48 +0530

    JS Data type

---
+++ js-learning/js-info/js_fundamentals/data types/dataType.js
@@ -0,0 +1,38 @@
+// In js every value has a certain type. like string or a number.
+
+// there are 7 primitive types and 1 non primitive
+
+/* 
+Primitive = primitive means that holds single value or we can say a natural one.
+
+non-primitive = Non-Primitive that hold a collection of data like arrays and objects
+
+# Primitive Types
+1. Number
+2. String
+3. BigInt
+4. Boolean
+5. null
+6. undefined
+7. symbol
+
+# Non-Primitive Type
+* Object
+
+
+Typeof operator allow us to see the type of a type
+
+let x = 'ashwin'
+
+typeof x || typeof(x) = string
+*/
+
+
+// Task
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
