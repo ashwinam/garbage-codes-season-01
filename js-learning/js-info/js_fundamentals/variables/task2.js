42ed3a98f357fbaf54b2187448bd1f6d38217c57
ashwinPy <ashwinam31@gmail.com>
2023-08-19 13:24:35 +0530

    Variable task 2 solutions

--- js-learning/js-info/js_fundamentals/variables/task2.js
+++ js-learning/js-info/js_fundamentals/variables/task2.js
@@ -3,7 +3,7 @@ importance: 3
 Create a variable with the name of our planet. How would you name such a variable?
 Create a variable to store the name of a current visitor to a website. How would you name that variable? */
 
-let planetEarth;
+let ourPlanetName = 'Earth';
 
 
-let currentVisitor = 'ashwin';
+let currentUserName = 'ashwin';
