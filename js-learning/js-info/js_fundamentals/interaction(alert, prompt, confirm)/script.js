7fa964ff1e726dd61dbce6e16c66546fac3afa0b
ashwinPy <ashwinam31@gmail.com>
2023-08-20 10:24:27 +0530

    Interaction(alert, prompt, confirm)

---
+++ js-learning/js-info/js_fundamentals/interaction(alert, prompt, confirm)/script.js
@@ -0,0 +1,17 @@
+/*
+alert
+shows a message.
+
+prompt
+shows a message asking the user to input text. It returns the text or, if Cancel button or Esc is clicked, null.
+
+confirm
+shows a message and waits for the user to press “OK” or “Cancel”. It returns true for OK and false for Cancel/Esc.
+
+*/
+
+// Task
+
+let user_name = prompt('What is your name?')
+
+alert(`Nice to meet you, ${user_name}`)
