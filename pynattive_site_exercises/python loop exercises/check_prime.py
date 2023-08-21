38634c5a712f5621750414f6897a55098322af0a
ashwinPy <ashwinam31@gmail.com>
2023-08-21 08:56:18 +0530

    loop exercise 11 other solution

--- pynattive_site_exercises/python loop exercises/check_prime.py
+++ pynattive_site_exercises/python loop exercises/check_prime.py
@@ -19,3 +19,23 @@ end_num = 50
 for num in range(start_num, end_num):
     if is_prime(num) is not None:
         print(is_prime(num))
+
+
+# other solution (pynative solution)
+
+start = 25
+end = 50
+print("Prime numbers between", start, "and", end, "are:")
+
+for num in range(start, end + 1):
+    # all prime numbers are greater than 1
+    # if number is less than or equal to 1, it is not prime
+    if num > 1:
+        for i in range(2, num):
+            # check for factors
+            if (num % i) == 0:
+                # not a prime number so break inner loop and
+                # look for next number
+                break
+        else:
+            print(num)
