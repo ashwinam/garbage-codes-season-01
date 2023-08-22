3a1cbdad646a40ddd285568920180173bb9c6e80
ashwinPy <ashwinam31@gmail.com>
2023-08-22 08:30:05 +0530

    loop Exercise 12

---
+++ pynattive_site_exercises/python loop exercises/calculate_fibonacci_series.py
@@ -0,0 +1,18 @@
+# Exercise 12: Display Fibonacci series up to 10 terms
+
+'''
+The Fibonacci Sequence is a series of numbers. The next number is found by adding up the two numbers before it. The first two numbers are 0 and 1.
+
+For example, 0, 1, 1, 2, 3, 5, 8, 13, 21. The next number in this series above is 13+21 = 34.
+
+
+'''
+
+pre_num = 0
+current_num = 1
+
+for num in range(10):
+    print(pre_num, end=', ')
+    total = pre_num + current_num
+    pre_num = current_num
+    current_num = total
