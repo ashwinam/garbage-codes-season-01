64b5668e75e0eed174f9363dfa582117a1f7e305
ashwinPy <ashwinam31@gmail.com>
2023-08-23 08:59:48 +0530

    js info basic operation day 2

--- js-learning/js-info/js_fundamentals/basic operations js/basic_operation.js
+++ js-learning/js-info/js_fundamentals/basic operations js/basic_operation.js
@@ -38,3 +38,35 @@ alert( +apples + +oranges ); // 5
 // alert( Number(apples) + Number(oranges) ); // 5
 
 */
+
+
+/*
+"" + 1 + 0
+'10'
+"" - 1 + 0
+-1
+true + false
+1
+6 / "3"
+2
+"2" * "3"
+6
+4 + 5 + "px"
+'9px'
+"$" + 4 + 5
+'$45'
+"4" - 2
+2
+"4px" - 2
+NaN
+"  -9  " + 5
+'  -9  5'
+"  -9  " - 5
+-14
+null + 1
+1
+undefined + 1
+NaN
+" \t \n" - 2
+-2
+*/
