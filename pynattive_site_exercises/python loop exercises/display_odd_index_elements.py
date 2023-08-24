e4e8c7e0710aa2f0334becf80c627a4a7d1225eb
ashwinPy <ashwinam31@gmail.com>
2023-08-24 08:31:21 +0530

    loop exercise 15

---
+++ pynattive_site_exercises/python loop exercises/display_odd_index_elements.py
@@ -0,0 +1,7 @@
+# Exercise 15: Use a loop to display elements from a given list present at odd index positions
+
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+for ind, ele in enumerate(my_list):
+    if ind % 2 != 0:
+        print(ele, end=' ')
