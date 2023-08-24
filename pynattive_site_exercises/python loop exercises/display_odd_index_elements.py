cd6ce9b7ca0ee2784212a43b896a9effbc2a1b71
ashwinPy <ashwinam31@gmail.com>
2023-08-24 08:34:09 +0530

    loop exercise 15 other solution

--- pynattive_site_exercises/python loop exercises/display_odd_index_elements.py
+++ pynattive_site_exercises/python loop exercises/display_odd_index_elements.py
@@ -5,3 +5,9 @@ my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 for ind, ele in enumerate(my_list):
     if ind % 2 != 0:
         print(ele, end=' ')
+
+
+# Other solution using slicing
+
+for ele in my_list[1::2]:
+    print(ele, end=' - ')
