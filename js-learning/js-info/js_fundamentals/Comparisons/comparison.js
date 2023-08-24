705b2aed068b7d59b7d4522bd55ba127f3180be3
ashwinPy <ashwinam31@gmail.com>
2023-08-24 08:48:55 +0530

    comparision operators day 1

---
+++ js-learning/js-info/js_fundamentals/Comparisons/comparison.js
@@ -0,0 +1,17 @@
+// Comparison operators in js
+/*
+> -> greater
+>= -> greater than or equal to
+< -> less than
+<= -> less than or equal to
+== -> equal to 
+
+*** Comparision operators always return a Boolean type // true or false
+
+* while comparing different types it always convert it into the numerical type
+
+ex '01' == 1 // '01' --> 1
+
+*string comparison
+it compares 'Z' > 'A' = true  it uses dictionary or lexicography for comparing the strings
+*/
