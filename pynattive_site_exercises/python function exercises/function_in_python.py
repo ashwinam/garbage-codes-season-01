369f29bffed6c8fb93f573bf236cee751282c0f7
ashwinPy <ashwinam31@gmail.com>
2023-08-28 08:49:32 +0530

    function exercises 1

---
+++ pynattive_site_exercises/python function exercises/function_in_python.py
@@ -0,0 +1,12 @@
+# Exercise 1: Create a function in Python
+
+'''
+Write a program to create a function that takes two arguments, name and age, and print their value.
+'''
+
+
+def person(name, age):
+    print(f'This person name is {name} and the age is {age}.')
+
+
+person('ashwin', 28)
