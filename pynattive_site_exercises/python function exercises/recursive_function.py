c5d0243a6aa4e15d054d097e5a79ddad04cecb24
ashwinPy <ashwinam31@gmail.com>
2023-08-31 09:05:54 +0530

    function exercise 6

---
+++ pynattive_site_exercises/python function exercises/recursive_function.py
@@ -0,0 +1,18 @@
+# Exercise 6: Create a recursive function
+
+
+'''
+
+'''
+
+
+def addition(num):
+    if num:
+        # call same function by reducing number by 1
+        return num + addition(num - 1)
+    else:
+        return 0
+
+
+res = addition(10)
+print(res)
