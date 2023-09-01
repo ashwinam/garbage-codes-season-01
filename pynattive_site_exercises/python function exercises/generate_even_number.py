565449ac4af6e3cf6240076e1537189399ef3510
ashwinPy <ashwinam31@gmail.com>
2023-09-01 08:43:19 +0530

    function exercise 8

---
+++ pynattive_site_exercises/python function exercises/generate_even_number.py
@@ -0,0 +1,11 @@
+# Exercise 8: Generate a Python list of all the even numbers between 4 to 30
+
+'''
+range = 4 -- 30
+
+[num for num in range(4, 30) if num % 2 == 0]
+'''
+
+lst = [num for num in range(4, 30) if num % 2 == 0]
+
+print(lst)
