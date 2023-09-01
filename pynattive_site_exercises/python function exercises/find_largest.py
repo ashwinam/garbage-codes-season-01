a20f559cee727211054d2a882e1c8ca4f3b5ea5f
ashwinPy <ashwinam31@gmail.com>
2023-09-01 08:38:11 +0530

    function exercise 9

---
+++ pynattive_site_exercises/python function exercises/find_largest.py
@@ -0,0 +1,32 @@
+# Exercise 9: Find the largest item from a given list
+
+
+'''
+x = [4, 6, 8, 24, 12, 2]
+
+0 < 4 = 4
+4 < 6 = 6
+6 < 8 = 8
+8 < 24 = 24
+24 < 12 = -
+24 < 2 = -
+
+24
+
+'''
+
+
+def find_largest(lst):
+    largest = 0
+
+    for num in lst:
+        if largest < num:
+            largest = num
+
+    return largest
+
+
+x = [4, 6, 8, 24, 12, 2]
+result = find_largest(x)
+
+print(result)
