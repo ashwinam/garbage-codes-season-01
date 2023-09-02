7bae0a8b9305443f77d6b7c27b6490a7b819c66c
ashwinPy <ashwinam31@gmail.com>
2023-09-02 09:00:24 +0530

    string exercise 1A other solution

--- pynattive_site_exercises/python string exercices/create_firstMiddleLast_string.py
+++ pynattive_site_exercises/python string exercices/create_firstMiddleLast_string.py
@@ -23,3 +23,24 @@ def newString(string):
 result = newString("James")
 
 print(result)
+
+
+# Other solution
+
+str1 = 'James'
+print("Original String is", str1)
+
+# Get first character
+res = str1[0]
+
+# Get string size
+l = len(str1)
+# Get middle index number
+mi = int(l / 2)
+# Get middle character and add it to result
+res = res + str1[mi]
+
+# Get last character and add it to result
+res = res + str1[l - 1]
+
+print("New String:", res)
