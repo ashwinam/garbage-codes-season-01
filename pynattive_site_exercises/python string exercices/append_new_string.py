9cae73ce09a2d12d70484dc45115d9ddc2d0ea1e
ashwinPy <ashwinam31@gmail.com>
2023-09-02 09:34:04 +0530

    string exercise 2 Other solution

--- pynattive_site_exercises/python string exercices/append_new_string.py
+++ pynattive_site_exercises/python string exercices/append_new_string.py
@@ -37,3 +37,23 @@ def append_new_string(str1, str2):
 result = append_new_string('Auly', 'Kelly')
 
 print(result)
+
+# Other solution
+
+
+def append_middle(s1, s2):
+    print("Original Strings are", s1, s2)
+
+    # middle index number of s1
+    mi = int(len(s1) / 2)
+
+    # get character from 0 to the middle index number from s1
+    x = s1[:mi:]
+    # concatenate s2 to it
+    x = x + s2
+    # append remaining character from s1
+    x = x + s1[mi:]
+    print("After appending new string in middle:", x)
+
+
+append_middle("Ault", "Kelly")
