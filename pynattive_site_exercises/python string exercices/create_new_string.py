87cee5924de2c2c474ec97ee14e1a56ce168c383
ashwinPy <ashwinam31@gmail.com>
2023-09-03 09:41:36 +0530

    string exercise 3 other solution

--- pynattive_site_exercises/python string exercices/create_new_string.py
+++ pynattive_site_exercises/python string exercices/create_new_string.py
@@ -45,3 +45,25 @@ l_string = last_char_str(s1) + last_char_str(s2)
 new_str += l_string
 
 print(new_str)
+
+
+# Other solution
+def mix_string(s1, s2):
+    # get first character from both string
+    first_char = s1[0] + s2[0]
+
+    # get middle character from both string
+    middle_char = s1[int(len(s1) / 2):int(len(s1) / 2) + 1] + \
+        s2[int(len(s2) / 2):int(len(s2) / 2) + 1]
+
+    # get last character from both string
+    last_char = s1[len(s1) - 1] + s2[len(s2) - 1]
+
+    # add all
+    res = first_char + middle_char + last_char
+    print("Mix String is ", res)
+
+
+s1 = "America"
+s2 = "Japan"
+mix_string(s1, s2)
