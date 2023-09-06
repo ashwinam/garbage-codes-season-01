d5f902fd352ecd00fa794d5db06d8f9a383591fd
ashwinPy <ashwinam31@gmail.com>
2023-09-06 08:39:00 +0530

    string exercise 7 other solution

--- pynattive_site_exercises/python string exercices/string_balance_test.py
+++ pynattive_site_exercises/python string exercices/string_balance_test.py
@@ -5,3 +5,26 @@ s1 = "Ynf"
 s2 = "PYnative"
 
 print(s1 in s2)
+
+# Other solution
+
+
+def string_balance_test(s1, s2):
+    flag = True
+    for char in s1:
+        if char in s2:
+            continue
+        else:
+            flag = False
+    return flag
+
+
+s1 = "Yn"
+s2 = "PYnative"
+flag = string_balance_test(s1, s2)
+print("s1 and s2 are balanced:", flag)
+
+s1 = "Ynf"
+s2 = "PYnative"
+flag = string_balance_test(s1, s2)
+print("s1 and s2 are balanced:", flag)
