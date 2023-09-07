ac9cb05fb8810689023bc8571971b0fec6cb4832
ashwinPy <ashwinam31@gmail.com>
2023-09-07 08:51:35 +0530

    string exercise 8 other solution

--- pynattive_site_exercises/python string exercices/occurance_of_substring.py
+++ pynattive_site_exercises/python string exercices/occurance_of_substring.py
@@ -27,3 +27,15 @@ def finding_occurances_of_substring(string):
 
 
 finding_occurances_of_substring(str1)
+
+# Other solution
+
+str1 = "Welcome to USA. usa awesome, isn't it?"
+sub_string = "USA"
+
+# convert string to lowercase
+temp_str = str1.lower()
+
+# use count function
+count = temp_str.count(sub_string.lower())
+print("The USA count is:", count)
