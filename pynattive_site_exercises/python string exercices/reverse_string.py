f17b18a558a9bb283a675fd68474678a55558d52
ashwinPy <ashwinam31@gmail.com>
2023-09-09 08:30:38 +0530

    string exercise 11 using reversed

--- pynattive_site_exercises/python string exercices/reverse_string.py
+++ pynattive_site_exercises/python string exercices/reverse_string.py
@@ -7,3 +7,9 @@ for each_char in range(len(str1)-1, -1, -1):
     reverse_string += str1[each_char]
 
 print(reverse_string)
+
+
+# Solution using reversed
+result = ''.join(list(reversed(str1)))
+
+print(result)
