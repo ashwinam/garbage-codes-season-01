a26b8f10361fc7a64105d4f00b2b5d2414a81b09
ashwinPy <ashwinam31@gmail.com>
2023-09-09 08:38:54 +0530

    string exercise 12

---
+++ pynattive_site_exercises/python string exercices/last_occurance_given_string.py
@@ -0,0 +1,9 @@
+# Exercise 12: Find the last position of a given substring
+
+'''
+Write a program to find the last position of a substring “Emma” in a given string.
+'''
+
+str1 = "Emma is a data scientist who knows Python. Emma works at google."
+
+print(f'Last occurrence of Emma starts at index {str1.rfind("Emma")}')
