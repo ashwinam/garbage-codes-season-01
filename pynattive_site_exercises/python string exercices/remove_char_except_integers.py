aaa891942117df8f175bc9b51b73e806c27caed3
ashwinPy <ashwinam31@gmail.com>
2023-09-10 10:26:06 +0530

    string exercise 16 Other solution

--- pynattive_site_exercises/python string exercices/remove_char_except_integers.py
+++ pynattive_site_exercises/python string exercices/remove_char_except_integers.py
@@ -7,3 +7,14 @@ for char in str1:
         result += char
 
 print(result)
+
+# Other solution
+
+str1 = 'I am 25 years and 10 months old'
+print("Original string is", str1)
+
+# Retain Numbers in String
+# Using list comprehension + join() + isdigit()
+res = "".join([item for item in str1 if item.isdigit()])
+
+print(res)
