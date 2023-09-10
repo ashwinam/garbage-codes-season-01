e583c055d9bd2b144c78df50d50e3d099ad2da12
ashwinPy <ashwinam31@gmail.com>
2023-09-10 10:39:50 +0530

    string exercise 18 Other solution

--- pynattive_site_exercises/python string exercices/replace_char.py
+++ pynattive_site_exercises/python string exercices/replace_char.py
@@ -15,3 +15,19 @@ for char in str1:
         result += char
 
 print(result)
+
+
+# Other solution
+
+
+str1 = '/*Jon is @developer & musician!!'
+print("The original string is : ", str1)
+
+# Replace punctuations with #
+replace_char = '#'
+
+# string.punctuation to get the list of all special symbols
+for char in string.punctuation:
+    str1 = str1.replace(char, replace_char)
+
+print("The strings after replacement : ", str1)
