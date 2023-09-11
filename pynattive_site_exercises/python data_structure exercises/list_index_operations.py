ee9441ea8cd7295c4bd28fc34e9801a61e726b73
ashwinPy <ashwinam31@gmail.com>
2023-09-11 08:45:40 +0530

    data structure exercise 02

---
+++ pynattive_site_exercises/python data_structure exercises/list_index_operations.py
@@ -0,0 +1,17 @@
+# Exercise 2: Remove and add item in a list
+
+
+'''
+Write a program to remove the item present at index 4 and add it to the 2nd position and at the end of the list.
+'''
+
+list1 = [54, 44, 27, 79, 91, 41]
+
+list1.pop(4)
+print(list1)
+
+list1.insert(2, 11)
+print(list1)
+
+list1.append(11)
+print(list1)
