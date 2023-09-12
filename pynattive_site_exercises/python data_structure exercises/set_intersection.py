d02724578de694a9d8ab94132285e406c429a4ad
ashwinPy <ashwinam31@gmail.com>
2023-09-12 08:56:55 +0530

    ds exercise 6 other solution

--- pynattive_site_exercises/python data_structure exercises/set_intersection.py
+++ pynattive_site_exercises/python data_structure exercises/set_intersection.py
@@ -9,3 +9,18 @@ intersected_set = first_set.intersection(second_set)
 first_set.difference_update(second_set)
 
 print(first_set)
+
+# Other solution
+
+first_set = {23, 42, 65, 57, 78, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+print("First Set ", first_set)
+print("Second Set ", second_set)
+
+intersection = first_set.intersection(second_set)
+print("Intersection is ", intersection)
+for item in intersection:
+    first_set.remove(item)
+
+print("First Set after removing common element ", first_set)
