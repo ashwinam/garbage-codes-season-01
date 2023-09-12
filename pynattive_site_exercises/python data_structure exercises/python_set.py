7f9fc4b041f640a672d0ae3d2fa8eb7be30dad68
ashwinPy <ashwinam31@gmail.com>
2023-09-12 08:48:34 +0530

    ds exercise 05

---
+++ pynattive_site_exercises/python data_structure exercises/python_set.py
@@ -0,0 +1,20 @@
+# Exercise 5: Create a Python set such that it shows the element from both lists in a pair
+
+'''
+first_list = [2, 3, 4, 5, 6, 7, 8]
+second_list = [4, 9, 16, 25, 36, 49, 64]
+
+expected:
+Result is  {(6, 36), (8, 64), (4, 16), (5, 25), (3, 9), (7, 49), (2, 4)}
+'''
+
+
+first_list = [2, 3, 4, 5, 6, 7, 8]
+second_list = [4, 9, 16, 25, 36, 49, 64]
+
+result = set()
+
+for f_num, s_num in zip(first_list, second_list):
+    result.add((f_num, s_num))
+
+print('Result is ', result)
