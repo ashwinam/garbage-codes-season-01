05d0683b19ad24ba40d781c2c79159de30672bf4
ashwinPy <ashwinam31@gmail.com>
2023-09-13 08:55:32 +0530

    ds exercise 3 incomplete

--- pynattive_site_exercises/python data_structure exercises/slice_3_parts.py
+++ pynattive_site_exercises/python data_structure exercises/slice_3_parts.py
@@ -10,5 +10,6 @@
 sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
 length = len(sample_list) / 3
 
-for _ in range(3):
-    print('*')
+for _ in range(0, 10):
+    if _ % 3 == 0:
+        print(_)
