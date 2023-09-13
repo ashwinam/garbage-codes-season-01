b3edf79ff14f717047bfdce8f2a782d80df0493d
ashwinPy <ashwinam31@gmail.com>
2023-09-13 08:54:50 +0530

    ds exercise 3 incomplete

---
+++ pynattive_site_exercises/python data_structure exercises/slice_3_parts.py
@@ -0,0 +1,14 @@
+# Exercise 3: Slice list into 3 equal chunks and reverse each chunk
+
+
+'''
+9 item
+0: 3, 
+3: 6, 
+6: 9
+'''
+sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+length = len(sample_list) / 3
+
+for _ in range(3):
+    print('*')
