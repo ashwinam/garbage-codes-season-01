5a0d4b7b6d0f94e1af41ca858822877e80bccebf
ashwinPy <ashwinam31@gmail.com>
2023-09-15 07:44:49 +0530

    ds exercise 8 other solution

--- pynattive_site_exercises/python data_structure exercises/check_element_exists.py
+++ pynattive_site_exercises/python data_structure exercises/check_element_exists.py
@@ -10,3 +10,16 @@ for number in roll_number:
         roll_number.remove(number)
 
 print(roll_number)
+
+
+# Other solution
+
+roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+sample_dict = {'Jhon': 47, 'Emma': 69, 'Kelly': 76, 'Jason': 97}
+
+print("List:", roll_number)
+print("Dictionary:", sample_dict)
+
+# create new list
+roll_number[:] = [item for item in roll_number if item in sample_dict.values()]
+print("after removing unwanted elements from list:", roll_number)
