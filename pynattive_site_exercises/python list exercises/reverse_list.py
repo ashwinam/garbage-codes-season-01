f253daedf978b9ec5c0246fee722344cad489432
ashwinPy <ashwinam31@gmail.com>
2023-09-17 10:16:51 +0530

    list exercise 01

---
+++ pynattive_site_exercises/python list exercises/reverse_list.py
@@ -0,0 +1,16 @@
+# Exercise 1: Reverse a list in Python
+
+
+'''
+1. take a list, use a reverse method for reversing list
+
+2. using slicing, to reverse a list
+
+3. loop reverse and then using index to reversing a list
+'''
+
+list1 = [100, 200, 300, 400, 500]
+
+print(list1.reverse())
+
+print(list1)
