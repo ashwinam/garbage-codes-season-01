b01fac5f7605f14ba130caedae447f73ea113534
ashwinPy <ashwinam31@gmail.com>
2023-09-18 21:02:33 +0530

    list exercise 02 other solution

--- pynattive_site_exercises/python list exercises/concatenate_two_list.py
+++ pynattive_site_exercises/python list exercises/concatenate_two_list.py
@@ -27,3 +27,11 @@ for ind in range(lst):
         list3.append(list2[ind])
 
 print(list3)
+
+
+# Other solution
+
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+list3 = [i + j for i, j in zip(list1, list2)]
+print(list3)
