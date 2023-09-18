4141894f176f47fcfb528a0972c80676f6e1ee8e
ashwinPy <ashwinam31@gmail.com>
2023-09-18 21:00:19 +0530

    list exercise 02 complete

--- pynattive_site_exercises/python list exercises/concatenate_two_list.py
+++ pynattive_site_exercises/python list exercises/concatenate_two_list.py
@@ -4,8 +4,8 @@
 Write a program to add two lists index-wise. Create a new list that contains the 0th index item from both the list, then the 1st index item, and so on till the last element. any leftover items will get added at the end of the new list.
 '''
 
-list1 = ["M", "na", "i", "Ke", '123']
-list2 = ["y", "me", "s", "lly"]
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly", "don't"]
 
 list3 = []
 
@@ -19,9 +19,9 @@ else:
     lst = len(list2)
 
 for ind in range(lst):
-    if lst1_length >= (ind - 1) and lst2_length >= (ind - 1):
+    if lst1_length > ind and lst2_length > ind:
         list3.append(list1[ind]+list2[ind])
-    elif lst1_length >= (ind - 1):
+    elif lst1_length > ind:
         list3.append(list1[ind])
     else:
         list3.append(list2[ind])
