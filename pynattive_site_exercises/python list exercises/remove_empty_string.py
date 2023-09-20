c93c9dbf4b618572617274dd2fc4b11d4895b826
ashwinPy <ashwinam31@gmail.com>
2023-09-20 08:54:17 +0530

    list exercise 06 other solution

--- pynattive_site_exercises/python list exercises/remove_empty_string.py
+++ pynattive_site_exercises/python list exercises/remove_empty_string.py
@@ -11,3 +11,11 @@ print(list(filter(lambda x: x, list1)))
 
 # using list comprehension
 print([ele for ele in list1 if ele])
+
+# Other solution
+
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+
+# remove None from list1 and convert result into list
+res = list(filter(None, list1))
+print(res)
