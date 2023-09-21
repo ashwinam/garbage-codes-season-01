0fd2659aea5f2c30e4ac2964891fbecf06124dff
ashwinPy <ashwinam31@gmail.com>
2023-09-21 09:01:30 +0530

    list exercise 07 incomplete

---
+++ pynattive_site_exercises/python list exercises/add_new_item.py
@@ -0,0 +1,35 @@
+# Exercise 7: Add new item to list after a specified item
+
+'''
+Write a program to add item 7000 after 6000 in the following Python List
+
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+
+[10, 20, [300, 400, [5000, 6000, 7000], 500], 30, 40]
+'''
+
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+
+temp_list = []
+
+for lst in list1:
+    if isinstance(lst, list):
+        if 6000 in lst:
+            lst.append(7000)
+            temp_list.append(lst)
+        else:
+            for row in lst:
+                if isinstance(row, list):
+                    if 6000 in row:
+                        row.append(7000)
+                        temp_list.append(lst)
+                    else:
+                        lst.append(row)
+                        temp_list.append(lst)
+                else:
+                    lst.append(row)
+                    temp_list.append(lst)
+    else:
+        temp_list.append(lst)
+
+print(temp_list, '>>>>')
