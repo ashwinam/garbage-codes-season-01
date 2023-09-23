d6e42f0f254f1a03bf4412e1ac03d79473f4c265
ashwinPy <ashwinam31@gmail.com>
2023-09-23 08:35:34 +0530

    list exercise 10

---
+++ pynattive_site_exercises/python list exercises/remove_specific_items.py
@@ -0,0 +1,12 @@
+# Exercise 10: Remove all occurrences of a specific item from a list.
+
+'''
+Given a Python list, write a program to remove all occurrences of item 20.
+'''
+
+list1 = [5, 20, 15, 20, 25, 50, 20]
+
+while 20 in list1:
+    list1.remove(20)
+
+print(list1)
