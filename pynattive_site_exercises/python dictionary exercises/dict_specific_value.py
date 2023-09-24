6c946d47ed5931414d95a76de4251db68260f00a
ashwinPy <ashwinam31@gmail.com>
2023-09-24 09:01:28 +0530

    dict exercise 03 extra context

--- pynattive_site_exercises/python dictionary exercises/dict_specific_value.py
+++ pynattive_site_exercises/python dictionary exercises/dict_specific_value.py
@@ -15,6 +15,11 @@ sampleDict = {
 '''
 unpacking values
 sample_dict['class']['student']['marks']['history']
+# understand how to located the nested key
+# sampleDict['class'] = {'student': {'name': 'Mike', 'marks': {'physics': 70, 'history': 80}}}
+# sampleDict['class']['student'] = {'name': 'Mike', 'marks': {'physics': 70, 'history': 80}}
+# sampleDict['class']['student']['marks'] = {'physics': 70, 'history': 80}
+
 '''
 
 print(sampleDict['class']['student']['marks']['history'])
