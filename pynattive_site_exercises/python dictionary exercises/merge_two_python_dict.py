1f146dfd6102dc63c76b78a1a47a7071772a141b
ashwinPy <ashwinam31@gmail.com>
2023-09-24 08:43:23 +0530

    dict exercise 02 using loop

--- pynattive_site_exercises/python dictionary exercises/merge_two_python_dict.py
+++ pynattive_site_exercises/python dictionary exercises/merge_two_python_dict.py
@@ -8,5 +8,12 @@ dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
 dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
 
-dict1.update(dict2)
+# dict1.update(dict2)
+print(dict1)
+
+# using loop
+
+for k, v in dict2.items():
+    dict1[k] = v
+
 print(dict1)
