2bcc06ac26452c03e4e5ac86cdac681866a81f1a
ashwinPy <ashwinam31@gmail.com>
2023-09-24 08:44:49 +0530

    dict exercise 02 other solution

--- pynattive_site_exercises/python dictionary exercises/merge_two_python_dict.py
+++ pynattive_site_exercises/python dictionary exercises/merge_two_python_dict.py
@@ -8,7 +8,7 @@ dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
 dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
 
-# dict1.update(dict2)
+dict1.update(dict2)
 print(dict1)
 
 # using loop
@@ -17,3 +17,11 @@ for k, v in dict2.items():
     dict1[k] = v
 
 print(dict1)
+
+# other version
+
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+dict3 = {**dict1, **dict2}
+print(dict3)
