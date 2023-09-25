96b4cc94d194cffbd946a80c41650accb8b84e43
ashwinPy <ashwinam31@gmail.com>
2023-09-25 08:53:28 +0530

    dictionary exercise 05 other solution

--- pynattive_site_exercises/python dictionary exercises/create_dict_by_extracting_key.py
+++ pynattive_site_exercises/python dictionary exercises/create_dict_by_extracting_key.py
@@ -20,3 +20,23 @@ for key in keys:
         res[key] = sample_dict[key]
 
 print(res)
+
+
+# Other solution
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# keys to extract
+keys = ["name", "salary"]
+
+# new dict
+res = dict()
+
+for k in keys:
+    # add current key with its va;ue from sample_dict
+    res.update({k: sample_dict[k]})
+print(res)
