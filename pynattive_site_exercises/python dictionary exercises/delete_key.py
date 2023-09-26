97d44570ec67b3eb2d0901e10fbedf78a4b04625
ashwinPy <ashwinam31@gmail.com>
2023-09-26 08:36:51 +0530

    dict exercise 06 other solution

--- pynattive_site_exercises/python dictionary exercises/delete_key.py
+++ pynattive_site_exercises/python dictionary exercises/delete_key.py
@@ -16,3 +16,18 @@ keys = ["name", "salary"]
 # print(sample_dict)
 
 print({k: v for k, v in sample_dict.items() if k not in keys})
+
+# Other solution
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+# Keys to remove
+keys = ["name", "salary"]
+
+for k in keys:
+    sample_dict.pop(k)
+print(sample_dict)
