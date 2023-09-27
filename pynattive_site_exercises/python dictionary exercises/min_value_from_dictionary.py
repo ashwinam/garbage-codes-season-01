64a66b55f4d915835cbd7ef93e2b8263fd8bec17
ashwinPy <ashwinam31@gmail.com>
2023-09-27 08:39:38 +0530

    dict exercise 09

---
+++ pynattive_site_exercises/python dictionary exercises/min_value_from_dictionary.py
@@ -0,0 +1,13 @@
+# Exercise 9: Get the key of a minimum value from the following dictionary
+
+sample_dict = {
+    'Physics': 82,
+    'Math': 65,
+    'history': 75
+}
+
+min_value = min(sample_dict.values())
+
+for k, v in sample_dict.items():
+    if v == min_value:
+        print(k)
