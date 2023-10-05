d100434bfa726e1f2a0803e2fc630b155565549d
ashwinPy <ashwinam31@gmail.com>
2023-10-05 09:03:46 +0530

    sets exercise 05

---
+++ pynattive_site_exercises/python sets exercises/remove_items_from_sets.py
@@ -0,0 +1,12 @@
+# Exercise 5: Remove items from the set at once
+
+'''
+Write a Python program to remove items 10, 20, 30 from the following set at once.
+'''
+
+set1 = {10, 20, 30, 40, 50}
+
+for i in [10, 20, 30]:
+    set1.discard(i)
+
+print(set1)
