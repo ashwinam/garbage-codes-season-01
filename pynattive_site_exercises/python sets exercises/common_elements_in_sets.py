174eda68f0abd738d885c679a5a38b05d80b20b8
ashwinPy <ashwinam31@gmail.com>
2023-10-06 08:51:59 +0530

    set exercise 7

---
+++ pynattive_site_exercises/python sets exercises/common_elements_in_sets.py
@@ -0,0 +1,6 @@
+# Exercise 7: Check if two sets have any elements in common. If yes, display the common elements
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {60, 70, 80, 90, 10}
+
+print(set1.intersection(set2))
