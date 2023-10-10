58f1438ad1a199cba62ecda9759c0359592d17fb
ashwinPy <ashwinam31@gmail.com>
2023-10-10 08:43:55 +0530

    tuple exercise 02

---
+++ pynattive_site_exercises/python tuples exercise/access_value.py
@@ -0,0 +1,9 @@
+# Exercise 2: Access value 20 from the tuple
+
+'''
+The given tuple is a nested tuple. write a Python program to print the value 20.
+'''
+
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+
+print(tuple1[1][1])
