280b58dd5eafeb0f5e223d8f7d6db4babb614058
ashwinPy <ashwinam31@gmail.com>
2023-10-11 08:25:53 +0530

    tuple exercise 04

---
+++ pynattive_site_exercises/python tuples exercise/unpacking_tuple.py
@@ -0,0 +1,16 @@
+# Exercise 4: Unpack the tuple into 4 variables
+
+'''
+Write a program to unpack the following tuple into four variables and display each variable.
+'''
+
+
+tuple1 = (10, 20, 30, 40)
+
+a, b, c, d = tuple1
+
+# Your code
+print(a) # should print 10
+print(b) # should print 20
+print(c) # should print 30
+print(d) # should print 40
