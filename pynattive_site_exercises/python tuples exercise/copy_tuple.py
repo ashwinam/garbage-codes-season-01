15d6ae1263d4cda1758d4f043b7d39db52ebea59
ashwinPy <ashwinam31@gmail.com>
2023-10-12 08:39:04 +0530

    tuple exercise 6 other solution

--- pynattive_site_exercises/python tuples exercise/copy_tuple.py
+++ pynattive_site_exercises/python tuples exercise/copy_tuple.py
@@ -8,3 +8,9 @@ tuple1 = (11, 22, 33, 44, 55, 66)
 tuple2 = tuple1[tuple1.index(44)], tuple1[tuple1.index(55)]
 
 print(tuple2)
+
+# Other solution
+
+tuple1 = (11, 22, 33, 44, 55, 66)
+tuple2 = tuple1[3:-1]
+print(tuple2)
