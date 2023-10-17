6c245043cd9ab00c9e6c9f62b7c7a345e77d614d
ashwinPy <ashwinam31@gmail.com>
2023-10-17 08:11:18 +0530

    datetime exercise 03

---
+++ pynattive_site_exercises/python date_time exercises/subtract_week.py
@@ -0,0 +1,7 @@
+# Exercise 3: Subtract a week (7 days)  from a given date in Python
+
+from datetime import datetime, timedelta
+
+given_date = datetime(2020, 2, 25)
+
+print(given_date - timedelta(days=7))
