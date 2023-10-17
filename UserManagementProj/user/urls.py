35942735741b9f4be3d166c987736d76acfb2eaf
ashwinPy <ashwinam31@gmail.com>
2023-10-17 08:31:25 +0530

    Login, Logout Functionality

--- UserManagementProj/user/urls.py
+++ UserManagementProj/user/urls.py
@@ -1,6 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from user.views import dashboard
 
+
+'''
+    path('accounts/', include("django.contrib.auth.urls")),
+    this will give you access to all of the following URLs:
+    1. accounts/login
+    2. accounts/logout
+    3. accounts/password_change
+    4. accounts/password_change/done
+    5. accounts/password_reset/
+    6. accounts/password_reset/done
+    7. accounts/reset/<uidb64>/<token>/
+    8. accounts/reset/done/
+    '''
 urlpatterns = [
+    path('accounts/', include("django.contrib.auth.urls")),
     path("dashboard/", dashboard, name="dashboard"),
 ]
