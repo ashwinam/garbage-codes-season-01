9579df3a888748473e647ebcf3fdea25f8e1956b
ashwinPy <ashwinam31@gmail.com>
2023-10-18 08:32:00 +0530

    datetime exercise 05 Other solution

--- pynattive_site_exercises/python date_time exercises/find_day.py
+++ pynattive_site_exercises/python date_time exercises/find_day.py
@@ -7,3 +7,11 @@ print(given_date.today().weekday())
 
 print(given_date.strftime('%A'))
 
+
+# Other solution
+import calendar
+from datetime import datetime
+
+given_date = datetime(2020, 7, 26)
+weekday = calendar.day_name[given_date.weekday()]
+print(weekday)
