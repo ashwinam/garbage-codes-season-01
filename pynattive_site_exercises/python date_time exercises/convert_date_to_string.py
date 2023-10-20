ef89cf806fa2f80025e1958fe9e0e900d5053fc2
ashwinPy <ashwinam31@gmail.com>
2023-10-20 08:28:38 +0530

    datetime exercise 08 other solution

--- pynattive_site_exercises/python date_time exercises/convert_date_to_string.py
+++ pynattive_site_exercises/python date_time exercises/convert_date_to_string.py
@@ -6,3 +6,11 @@ from datetime import datetime
 given_date = datetime(2020, 2, 25)
 
 print(str(given_date))
+
+
+# Other solution
+from datetime import datetime
+
+given_date = datetime(2020, 2, 25)
+string_date = given_date.strftime("%Y-%m-%d %H:%M:%S")
+print(string_date)
