3445e8c7cbde5e694761cf4185b0a55bb4fda67d
ashwinPy <ashwinam31@gmail.com>
2023-10-24 08:53:38 +0530

    outlines

---
+++ Oop Learning/oop.py
@@ -0,0 +1,11 @@
+'''
+Outline:
+1. Create classes
+    a.Why, How
+    b.class vs instance
+2. Functions inside classes
+3. Inheritance(Base + child class)
+4. Encapsulation (Private/Public)
+5. Properties(Getter/Setter)
+'''
+
