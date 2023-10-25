2085dd37e01244fb1031713318d5dac42646375d
ashwinPy <ashwinam31@gmail.com>
2023-10-25 20:47:52 +0530

    simple create operation

--- DjangoToDo/todo/views.py
+++ DjangoToDo/todo/views.py
@@ -11,3 +11,15 @@ class ToDoView(ListView):
     model = ToDoTbl
     template_name = 'todo/todo.html'
     context_object_name = "objects"
+
+    def post(self, *args, **kwargs):
+        todo_name = self.request.POST.get('todo_name', None)
+        todo_desc = self.request.POST.get('todo_description', None)
+        
+        # Object creation.
+        obj = ToDoTbl(todo_name=todo_name, todo_description=todo_desc)
+        obj.save()
+
+        queryset = self.model.objects.all()
+
+        return render(self.request, 'todo/todo.html', {'objects': queryset})
