652a09958f9d502260534cdde7deee4ba7be0075
ashwinPy <ashwinam31@gmail.com>
2023-11-05 23:23:42 +0530

    changes

--- DjangoToDo/users/urls.py
+++ DjangoToDo/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path("", include("django.contrib.auth.urls")),
+    path("", include("django.contrib.auth.urls")),
     path("login/", views.UserLoginView.as_view(), name='login'),
 ]
