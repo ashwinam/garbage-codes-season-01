98185600fb943b6a4ad729ac8bae5597298f2464
ashwinPy <ashwinam31@gmail.com>
2023-11-22 12:01:11 +0530

    psycopg2-binary

--- DjangoToDo/DjangoToDo/settings.py
+++ DjangoToDo/DjangoToDo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,6 +82,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.parse('postgres://todo_nation_db_user:nZgBSMC0tdycWAB9OynwXU0sqMNCh01U@dpg-clepod53qkas73avk830-a.singapore-postgres.render.com/todo_nation_db')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
